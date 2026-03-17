dd55f9add0049bc222060af1bb9ec4e0de33903c
Adeenfatimah <adeenfatimahhh@gmail.com>
2026-03-17 10:11:51 +0500

    first commit

---
+++ chap 4/app.js
@@ -0,0 +1,38 @@
+// Ques 1 
+// var name = "Ali", age = 20, city = "Karachi";
+
+// Ques 2
+
+// Legal Variable Names
+// var myName;
+// var _age;
+// var $price;
+// var user1;
+// var first_name;
+
+// Illegal Variable Names
+// var 1name;        // cannot start with number
+// var my-name;      // hyphen not allowed
+// var var;          // reserved keyword
+// var first name;   // space not allowed
+// var @value;       // special character not allowed
+
+// Ques 3
+
+// a 
+// document.write("<h1>Rules for naming JS variables</h1>");
+
+// b 
+// document.write("Variable names can only contain letters, numbers, $ and _. For example: $my_1stVariable");
+// document.write("<br />")
+
+// c 
+// document.write("Variables must begin with a letter, $ or _. For example $name, _name or name ");
+// document.write("<br />")
+
+// d 
+// document.write("Variable names are case sensitive");
+// document.write("<br />")
+
+// e 
+// document.write("Variable names should not be JS keywords.");
